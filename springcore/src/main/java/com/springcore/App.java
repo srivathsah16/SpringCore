cc203b89c8b2b3f1b5372af59385a8c422f1f149
srivath <srivath@LIN76004267.corp.capgemini.com>
2023-05-31 22:11:36 +0530

    secondCommit

--- springcore/src/main/java/com/springcore/App.java
+++ springcore/src/main/java/com/springcore/App.java
@@ -7,11 +7,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * Hello world!
  *
  */
-public class App {  
+public class App { 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
 		Student student1 = (Student) context.getBean("Student");
 		System.out.println(student1);
+
 	}
 }
--- springcore/src/main/java/com/springcore/Student.java
+++ springcore/src/main/java/com/springcore/Student.java
@@ -1,7 +1,7 @@
 package com.springcore;
 
-public class Student { 
-	private int id; 
+public class Student {
+	private int id;
 	private String name;
 	private String address;
 
